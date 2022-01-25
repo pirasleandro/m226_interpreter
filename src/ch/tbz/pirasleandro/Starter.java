edf17daf4b2bdccba34ee26c02f45bb5573df854
data-corrupted <leandro.piras@noseryoung.ch>
2022-01-25 10:51:34 +0100

    added proof of concept (AST object of input code)

--- src/ch/tbz/pirasleandro/Starter.java
+++ src/ch/tbz/pirasleandro/Starter.java
@@ -1,6 +1,10 @@
 package ch.tbz.pirasleandro;
 
 import java.util.ArrayList;
+import ch.tbz.pirasleandro.AST.Line.*;
+import ch.tbz.pirasleandro.AST.Block.*;
+import ch.tbz.pirasleandro.AST.Expression.*;
+import ch.tbz.pirasleandro.AST.Expression.Comparison.ComparisonType;
 
 public class Starter {
     public static void main(String[] args) {
@@ -8,18 +12,38 @@ public class Starter {
             "HAI 1.3" + "\n" +
             "   VISIBLE \"Hai world!\"\n" +
             "   I HAS A uwu ITZ \"OWO\"\n" +
-            "   I HAS A heehee ITZ \"HEEHEE\"\n" +
-            "   I HAS A aBC ITZ 1\n" +
-            "   I HAS A ABc ITZ 2\n" +
-            "   I HAS A a1c ITZ 3\n" +
-            "   I HAS A NAW ITZ 4\n" +
-            "   OBTW\n" +
-            "       commentline1\n" +
-            "       COMMENTLINE2\n" +
-            "       commentLINE3\n" +
-            "   TLDR\n" +
+            "   VISIBLE uwu\n" +
+            "   BOTH SAEM uwu AN \"uwu\", O RLY?\n" +
+            "   YA RLY\n" +
+            "       VISIBLE \"true\"\n" +
+            "   NO WAI\n" +
+            "       VISIBLE \"false\"\n" +
+            "   OIC\n" +
             "KTHXBYE"
         );
+
+        // Abstract syntax tree for code above
+        @SuppressWarnings("unused")
+        AST[] code = {
+            new Version("1.3"),
+            new Print(new Value<String>("Hai World!!!1!!")),
+            new VariableDeclaration("uwu", new Value<String>("OWO")),
+            new Print(new VariableReference("uwu")),
+            new IfStatement(
+                new Comparison(
+                    new VariableReference("uwu"),
+                    ComparisonType.EQUALS,
+                    new Value<String>("uwu")
+                ),
+                new AST[] {
+                    new Print(new Value<String>("true"))
+                },
+                new AST[] {
+                    new Print(new Value<String>("false"))
+                }
+            )
+        };
+        
         for (ArrayList<Token> line : lexer.lex()) {
             for (Token token : line) {
                 System.out.print(token.toString() + " ");
